4dc3bd30b70ba129bb22ae732e21ade7a06761f7
Jordan C <coker.jordan@gmail.com>
2025-07-25 00:59:09 -0400

    task 1 done

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,5 +19,31 @@ invCont.buildByClassificationId = async function (req, res, next) {
   })
 }
 
+// New: Build vehicle detail view
+invCont.buildByInvId = async function (req, res, next) {
+  const inv_id = req.params.invId;
+  try {
+    const data = await invModel.getInventoryById(inv_id);
+    if (data.length === 0) {
+      return res.status(404).render("errors/error", {
+        title: "Vehicle Not Found",
+        message: "Sorry, we couldn't find the vehicle you requested.",
+        nav: await utilities.getNav(),
+      });
+    }
+    const vehicle = data[0];
+    const detailHTML = utilities.buildVehicleDetail(vehicle);
+    const nav = await utilities.getNav();
+    res.render("inventory/detail", {
+      title: `${vehicle.inv_make} ${vehicle.inv_model}`,
+      nav,
+      detail: detailHTML,
+      vehicle,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 
 module.exports = invCont
--- utilities/index.js
+++ utilities/index.js
@@ -64,4 +64,25 @@ Util.buildClassificationGrid = async function(data){
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
+
+Util.buildVehicleDetail = async function (vehicle) {
+  if (!vehicle) return '<p class="notice">Vehicle information not available.</p>';
+
+  return `
+    <div class="vehicle-detail">
+      <div class="vehicle-image">
+        <img src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}" />
+      </div>
+      <div class="vehicle-info">
+        <h1>${vehicle.inv_make} ${vehicle.inv_model}</h1>
+        <p><strong>Year:</strong> ${vehicle.inv_year}</p>
+        <p><strong>Price:</strong> $${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</p>
+        <p><strong>Mileage:</strong> ${new Intl.NumberFormat('en-US').format(vehicle.inv_miles)} miles</p>
+        <p><strong>Description:</strong> ${vehicle.inv_description}</p>
+        <p><strong>Color:</strong> ${vehicle.inv_color}</p>
+      </div>
+    </div>
+  `;
+};
+
 module.exports = Util
