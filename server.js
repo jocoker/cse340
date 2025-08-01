6343410ce0d79f95cfce68b917a1ff8206c461de
Jordan C <coker.jordan@gmail.com>
2025-07-31 23:39:53 -0400

    NEW STUFF

--- server.js
+++ server.js
@@ -13,10 +13,33 @@ const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require('./utilities/index')
+const session = require("express-session")
+const pool = require('./database/')
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+
 
 /* ***********************
  * View Engine and Templates
  *************************/
+
+
+
+
+
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") //not at views root
