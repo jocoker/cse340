2d81f635e8e7436ee2647a3805d10c870efb6f60
Jordan C <coker.jordan@gmail.com>
2025-08-06 23:09:11 -0400

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,11 +2,20 @@ const express = require("express");
 const router = express.Router();
 const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
+const regValidate = require('../utilities/account-validation');
+
 
 // Route to build the login view at /account/login
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount));
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+)
+
 // Route to build the register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
